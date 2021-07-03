2e5229b1379f6dc56be083871858d3348abb3bfd
sarah-blazic <83601788+sarah-blazic@users.noreply.github.com>
2021-07-03 14:45:46 -0700

    fmt antivirus.tf

--- modules/sec_profiles/antivirus.tf
+++ modules/sec_profiles/antivirus.tf
@@ -1,6 +1,6 @@
 resource "panos_antivirus_security_profile" "this" {
   #for yaml files change jsondecode => yamldecode
-  for_each = var.antivirus_file != "optional" ? {for virus in jsondecode(file(var.antivirus_file)): virus.name => virus } : tomap({})
+  for_each = var.antivirus_file != "optional" ? { for virus in jsondecode(file(var.antivirus_file)) : virus.name => virus } : tomap({})
 
   name              = each.key
   device_group      = try(each.value.device_group, "shared")
@@ -11,9 +11,9 @@ resource "panos_antivirus_security_profile" "this" {
   dynamic "decoder" {
     for_each = lookup(each.value, "decoder", null) != null ? { for d in each.value.decoder : d.name => d } : {}
     content {
-      name                     = decoder.value.name
-      action                   = lookup(decoder.value, "action", "default")
-      wildfire_action          = lookup(decoder.value, "wildfire_action", "default")
+      name                    = decoder.value.name
+      action                  = lookup(decoder.value, "action", "default")
+      wildfire_action         = lookup(decoder.value, "wildfire_action", "default")
       machine_learning_action = try(decoder.value.machine_learning_action, null)
     }
   }
@@ -29,17 +29,17 @@ resource "panos_antivirus_security_profile" "this" {
   dynamic "machine_learning_model" {
     for_each = lookup(each.value, "machine_learning_model", null) != null ? { for mod in each.value.machine_learning_model : mod.model => mod } : {}
     content {
-      model = machine_learning_model.value.model
-      action = lookup(machine_learning_model.value, "action", "disable" )
+      model  = machine_learning_model.value.model
+      action = lookup(machine_learning_model.value, "action", "disable")
     }
   }
 
   dynamic "machine_learning_exception" {
     for_each = lookup(each.value, "machine_learning_exception", null) != null ? { for ex in each.value.machine_learning_exception : ex.name => ex } : {}
     content {
-      name = machine_learning_exception.value.name
-      description = lookup(machine_learning_exception.value, "description", null )
-      filename = lookup(machine_learning_exception.value, "filename", null )
+      name        = machine_learning_exception.value.name
+      description = lookup(machine_learning_exception.value, "description", null)
+      filename    = lookup(machine_learning_exception.value, "filename", null)
     }
   }
 
