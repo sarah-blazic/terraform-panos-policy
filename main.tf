2fdb189dd7f8d76e06c03ff427f13f615a2df1e7
sarah-blazic <83601788+sarah-blazic@users.noreply.github.com>
2021-07-06 12:12:24 -0700

    add vulnerability to sec profiles

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ module "sec_prof" {
   antivirus_file     = "./files/json/antivirus.json"
   file_blocking_file = "./files/json/file_blocking.json"
   wildfire_file      = "./files/json/wildfire.json"
+  vulnerability_file = "./files/json/vulnerability.json"
 }
 
 
