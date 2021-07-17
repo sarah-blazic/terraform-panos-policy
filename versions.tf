1aedb31f2b85299a24590cc26d3f03c15e4817bd
sarah-blazic <83601788+sarah-blazic@users.noreply.github.com>
2021-07-16 17:11:37 -0700

    backend

--- versions.tf
+++ versions.tf
@@ -4,5 +4,14 @@ terraform {
       source = "PaloAltoNetworks/panos"
     }
   }
+
+  backend "remote" {
+    organization = "Palo_Alto"
+
+    workspaces {
+      name = "trigger"
+    }
+  }
+
   required_version = ">= 0.13"
 }
