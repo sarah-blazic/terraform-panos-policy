062cee919130886a7ea54a596b92530f37e6e5cd
sarah-blazic <83601788+sarah-blazic@users.noreply.github.com>
2021-07-20 15:36:47 -0700

    fmt for publishing the module

--- examples/main.tf
+++ examples/main.tf
@@ -1,12 +1,19 @@
+provider "panos" {
+  hostname = "<panos_address>"
+  username = "<admin_username>"
+  password = "<admin_password>"
+}
+
 module "policy" {
-  source = "..\/modules\/policy"
+  source = "sarah-blazic/policy/panos"
+  version = "0.1.1"
 
   tags_file       = try(jsondecode(file("./files/json/tags.json")), {})
   services_file   = try(jsondecode(file("./files/json/services.json")), {})
   addr_group_file = try(jsondecode(file("./files/json/addr_group.json")), {})
   addr_obj_file   = try(jsondecode(file("./files/json/addr_obj.json")), {})
-//  sec_file        = try(jsondecode(file("./examples/json/sec_policy.json")), {})
-//  nat_file        = try(jsondecode(file("./examples/json/nat.json")), {})
+  sec_file        = try(jsondecode(file("./examples/json/sec_policy.json")), {})
+  nat_file        = try(jsondecode(file("./examples/json/nat.json")), {})
 }
 
 
